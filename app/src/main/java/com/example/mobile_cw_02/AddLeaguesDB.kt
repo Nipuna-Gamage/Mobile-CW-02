905af20827ca2ef5468ef30f1463075015d206d6
Nipuna-Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-04-28 22:53:06 +0530

    Add Activity

---
+++ app/src/main/java/com/example/mobile_cw_02/AddLeaguesDB.kt
@@ -0,0 +1,46 @@
+package com.example.mobile_cw_02
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile_cw_02.ui.theme.MobileCW02Theme
+
+class AddLeaguesDB : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileCW02Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    MobileCW02Theme {
+        Greeting2("Android")
+    }
+}
