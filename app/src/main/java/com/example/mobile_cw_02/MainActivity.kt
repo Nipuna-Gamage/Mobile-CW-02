86e24624f76d678bee55754202f4dd14ff3e53bd
Nipuna-Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-04-28 22:55:10 +0530

    modified main activity

--- app/src/main/java/com/example/mobile_cw_02/MainActivity.kt
+++ app/src/main/java/com/example/mobile_cw_02/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.mobile_cw_02
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mobile_cw_02.ui.theme.MobileCW02Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +28,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Button(onClick = { Intent( applicationContext, AddLeaguesDB::class.java).also { startActivity(it) } }) {
+                                Text(text = "Add Leagues to DB")
+                            }
+                            Button(onClick = { Intent(applicationContext, SearchClubs::class.java).also { startActivity(it) } }) {
+                                Text(text = "Search for Clubs By League")
+                            }
+                            Button(onClick = { Intent(applicationContext, SearchClubsLeague::class.java).also { startActivity(it) } }) {
+                                Text(text = "Search for Clubs")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileCW02Theme {
-        Greeting("Android")
     }
 }
