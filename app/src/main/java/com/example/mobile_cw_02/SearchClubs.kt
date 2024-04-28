f8653d179f55355217bb9446bdb2bea055fe9305
Nipuna-Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-04-28 23:14:32 +0530

    modified SearchClubs

--- app/src/main/java/com/example/mobile_cw_02/SearchClubs.kt
+++ app/src/main/java/com/example/mobile_cw_02/SearchClubs.kt
@@ -3,13 +3,32 @@ package com.example.mobile_cw_02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.mobile_cw_02.ui.theme.MobileCW02Theme
 
 class SearchClubs : ComponentActivity() {
@@ -22,7 +41,7 @@ class SearchClubs : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting3("Android")
+                    SearchClubsScreen()
                 }
             }
         }
@@ -30,17 +49,50 @@ class SearchClubs : ComponentActivity() {
 }
 
 @Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SearchClubsScreen(){
+    var leagueName by remember { mutableStateOf(TextFieldValue()) }
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = leagueName,
+            onValueChange = { leagueName = it },
+            label = { Text("Enter League Name") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+            })
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+            },
+            modifier = Modifier.height(Dp(40f))
+        ) {
+            Text("Retrieve Clubs")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /*saveClubsToDatabase(clubs)*/ },
+            modifier = Modifier.height(Dp(40f))
+        ) {
+            Text("Save Clubs to Database")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
+fun GreetingPreview() {
     MobileCW02Theme {
-        Greeting3("Android")
+        SearchClubsScreen()
     }
 }
